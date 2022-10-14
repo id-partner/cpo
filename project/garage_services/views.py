62d003a57a523a3c22d71fe0a7e6ffbaca81f933
Igor Drozdov <idcontext.ru@gmail.com>
2022-10-14 22:04:41 +0500

    Базовый шаблон. Include

--- project/garage_services/views.py
+++ project/garage_services/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
+def group_page(request):
     context = {}
-    return render(request, 'garage_services/index.html', context)
+    return render(request, 'garage_services/group_page.html', context)
