f979592fa97668604fbb45c8027bde7292d1fbbd
Igor Drozdov <idcontext.ru@gmail.com>
2022-10-19 18:28:15 +0500

    Summernote

--- garage_services/admin.py
+++ garage_services/admin.py
@@ -1,12 +1,38 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Service, Group, Brand, CarModel, Vehicle, ServicesVehicle, Engine, Transmission, Drive, Lead, ImageDesing
 
+class ServiceAdmin(SummernoteModelAdmin):
+    summernote_fields = (
+        'content',
+        )
 
-admin.site.register(Service)
-admin.site.register(Group)
-admin.site.register(Brand)
-admin.site.register(CarModel)
-admin.site.register(Vehicle)
+class GroupAdmin(SummernoteModelAdmin):
+    summernote_fields = (
+        'description',
+        )
+
+class BrandAdmin(SummernoteModelAdmin):
+    summernote_fields = (
+        'description',
+        )
+
+class CarModelAdmin(SummernoteModelAdmin):
+    summernote_fields = (
+        'description',
+        )
+
+class VehicleAdmin(SummernoteModelAdmin):
+    summernote_fields = (
+        'description',
+        )
+
+
+admin.site.register(Service, ServiceAdmin)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Brand, BrandAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+admin.site.register(Vehicle, VehicleAdmin)
 admin.site.register(ServicesVehicle)
 admin.site.register(Engine)
 admin.site.register(Transmission)
